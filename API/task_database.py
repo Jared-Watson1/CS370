223518d769e5f1fcbf37bf584191df6cf09672e7
Jared-Watson1 <jared.watson@emory.edu>
2023-09-24 14:21:24 -0400

    Set up tasks database hosting and basic operations
    Set up tasks database hosting and basic operations on table.
    
    Used ElephantSQL as host using their free tier. Created table which consists of: task_id, task_name, description, date_posted, and task_owner.
    
    also added functionality to add new tasks to table and to view all tasks currently posted.

--- API/task_database.py
+++ API/task_database.py
@@ -1,1 +1,98 @@
-# script for retrieval and updating of tasks database 
+import psycopg2
+import os
+from psycopg2 import errors
+from dotenv import load_dotenv
+from datetime import date
+
+
+load_dotenv()
+# connecting URL for database
+DATABASE_URL = os.getenv("TASK_DB_URL")
+
+# Create table for tasks
+def createTable():
+
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+
+    # SQL statement to create the tasks table
+    create_table_query = """
+    CREATE TABLE tasks (
+        task_id SERIAL PRIMARY KEY,
+        task_name VARCHAR(255) NOT NULL,
+        description TEXT,
+        date_posted DATE NOT NULL,
+        task_owner VARCHAR(255) NOT NULL
+    );
+    """
+
+    try:
+        cursor.execute(create_table_query)
+        conn.commit()
+        print("Table 'tasks' created successfully!")
+    except errors.DuplicateTable:
+        print("Table 'tasks' already exists!")
+    except Exception as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def add_task(task_name, description, date_posted, task_owner):
+
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+
+    # SQL statement to insert a new task
+    insert_task_query = """
+    INSERT INTO tasks (task_name, description, date_posted, task_owner)
+    VALUES (%s, %s, %s, %s);
+    """
+
+    try:
+        cursor.execute(insert_task_query, (task_name,
+                       description, date_posted, task_owner))
+        conn.commit()
+        print("Task added successfully!")
+    except Exception as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close()
+        conn.close()
+
+def get_all_tasks():
+    
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+    
+    # SQL statement to retrieve all tasks
+    select_all_tasks_query = "SELECT * FROM tasks;"
+
+    try:
+        cursor.execute(select_all_tasks_query)
+        tasks = cursor.fetchall()
+        
+        if not tasks:
+            print("No tasks found.")
+            return
+        
+        print("Tasks in the database:")
+        for task in tasks:
+            print(f"Task ID: {task[0]}, Task Name: {task[1]}, Description: {task[2]}, Date Posted: {task[3]}, Task Owner: {task[4]}")
+    except Exception as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close()
+        conn.close()
+
+# Example of retrieving and printing all tasks
+# get_all_tasks()
+
+
+# Example task
+# add_task("Sample Task", "This is a description for the sample task.",
+#          date.today(), "John Doe")
