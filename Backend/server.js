da56d72bfa7a1fb793fa7d1b7713cf0c4a65a067
xzhai8 <xzhai8@emory.edu>
2023-10-15 20:58:09 -0400

    Email ver
    email vertication

---
+++ Backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const sendGridMail = require('@sendgrid/mail');
+
+// Use your real SendGrid API Key here
+sendGridMail.setApiKey('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'); 
+
+const app = express();
+app.use(express.json());
+
+// Serve your HTML and other static assets
+app.use(express.static('frontend/templates'));
+
+// Send email verification when the relevant API endpoint is hit
+app.post('/api/send_verification', (req, res) => {
+    const { email } = req.body;
+
+    const msg = {
+        to: email,
+        from: 'johnzhaixingyu@gmail.com',  // Use your verified sender email
+        templateId: 'd-b97a373634f844f8a5b3106d5951085f',  // Use your actual template ID
+        dynamicTemplateData: {
+            email
+        }
+    };
+
+    sendGridMail.send(msg)
+        .then(() => {
+            res.status(200).send('Email sent successfully!');
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        });
+});
+
+// Handle requests to the root URL by serving your HTML file
+app.get('/', (req, res) => {
+    res.sendFile('forgot_password.html', { root: __dirname + '/frontend/templates/' });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/scripts/emailvrf.js
@@ -0,0 +1,24 @@
+async function login() {
+    const email = document.getElementById('username').value;
+    try {
+        const response = await fetch('/api/send_verification', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email})
+        });
+        if (response.ok) {
+            alert('Verification email sent. Please check your inbox.');
+        } else {
+            alert('Error sending email. Please try again later.');
+        }
+    } catch (error) {
+        alert('Request failed. Please try again later.');
+    }
+}
+
+document.getElementById('signupForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    login();
+});
+
+// /Users/xingyuzhai/Documents/GitHub/CS370/Backend/server.js
