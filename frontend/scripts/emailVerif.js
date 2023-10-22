6b8f66333da13ca44299805042753d6e1c3001e7
Dawit Dean <deand0629@gmail.com>
2023-10-22 19:51:25 -0400

    Update emailVerif.js
    made it function

--- frontend/scripts/emailVerif.js
+++ frontend/scripts/emailVerif.js
@@ -1,28 +1,31 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: 'DooleyAFavor@gmail.com',
-    pass: 'DooleySwoopEmory123',
-  },
-});
-
-const mailOptions = {
-  from: 'your-email@gmail.com',
-  to: 'recipient@example.com',
-  subject: 'Your Subject',
-  text: 'Your email content goes here.',
-};
-
-app.post('/send-email', (req, res) => {
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send('Error sending email');
-    } else {
-      console.log('Email sent: ' + info.response);
-      res.status(200).send('Email sent successfully');
-    }
+function email_verif(user_email) {
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'DooleyAFavor@gmail.com',
+      pass: 'DooleySwoopEmory123',
+    },
   });
-});
+  
+  const mailOptions = {
+    from: 'your-email@gmail.com',
+    to: 'recipient@example.com',
+    subject: 'Your Subject',
+    text: 'Your email content goes here.',
+  };
+  
+  app.post('/send-email', (req, res) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send('Error sending email');
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.status(200).send('Email sent successfully');
+      }
+    });
+  });
+}
+
